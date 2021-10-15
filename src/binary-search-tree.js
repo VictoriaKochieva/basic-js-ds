bc93397b04d54aa6adbe9870ebeedc50e52ee2f4
VictoriaKochieva <kochievavictoria@gmail.com>
2021-10-15 20:39:34 +0300

    feat: add part of solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,31 +1,75 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 module.exports = class BinarySearchTree {
+  constructor() {
+    this.treeRoot = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const node = new Node(data)
+
+    if(!this.treeRoot) {
+      this.treeRoot = node;
+      return
+    }
+
+    let currentNode = this.treeRoot
+
+    while(currentNode){
+      if(node.data < currentNode.data) {
+        if(!currentNode.left) {
+          currentNode.left = node
+          return 
+        }
+
+        currentNode = currentNode.left
+      }
+  
+      if(node.data > currentNode.data) {
+        if(!currentNode.right) {
+          currentNode.right = node
+          return
+        } 
+        currentNode = currentNode.right
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if(this.find(data)) {
+      return true
+    } else return false
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+
+    return findEl(this.treeRoot, data)
+
+    function findEl(node, data)  {
+      if(!node) {
+        return null
+      }
+      
+      if(node.data === data) {
+        return node
+      } 
+      
+      if(data < node.data) {
+        return findEl(node.left, data) 
+      } 
+
+      else return findEl(node.right, data) 
+    }
   }
 
   remove(/* data */) {
